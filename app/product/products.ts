548ca899a0a2d13397a4e6cbcc2eb481356f9ec1
naoiw <naoki.iwat@gmail.com>
2026-02-09 01:43:42 +0900

    add : サンプルプロダクトを追加

--- app/product/products.ts
+++ app/product/products.ts
@@ -5,4 +5,15 @@ export type Product = {
   image?: string // 例: "/products/my-app.png"
 }
 
-export const products: Product[] = []
+export const products: Product[] = [
+  {
+    name: 'sudoku-solver',
+    summary: '数独を解くツール',
+    url: 'https://naoiw.github.io/sudoku-solver/',
+  },
+  {
+    name: 'c17 code generator(開発中)',
+    summary: 'エプソン製マイコンc17シリーズの開発を補助するためのツール',
+    url: 'https://c17-code-generator.vercel.app/',
+  },
+]
